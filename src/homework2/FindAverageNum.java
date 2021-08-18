7182852de974ecb04587b16ed2e42a91a9f4e122
Atlantiks <atlantiks@bigmir.net>
2021-08-18 18:08:02 +0300

    Initial commit

---
+++ src/homework2/FindAverageNum.java
@@ -0,0 +1,12 @@
+package homework2;
+
+public class FindAverageNum {
+    public static void main(String[] args) {
+        int primaryNum = 10;
+        int secondaryNum = 20;
+
+        double averageNum = (primaryNum + secondaryNum) / 2.0;
+
+        System.out.println(averageNum);
+    }
+}
