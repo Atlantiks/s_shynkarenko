ba558d9d0a9c429d0acd406c079e56a95bbfae91
Atlantiks <atlantiks@bigmir.net>
2021-08-18 21:58:55 +0300

    Wrong data input processing included

--- src/homework2/BankPercentage.java
+++ src/homework2/BankPercentage.java
@@ -1,33 +1,60 @@
 package homework2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BankPercentage {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int termInYears = 0;
+        long initialDeposit = 0;
+        long finalDeposit = 0;
+        double interestRate = 0;
 
-        System.out.println("Enter, please, amount of your money for initial deposit: ");
+        do {
+            System.out.println("Enter, please, amount of your money for initial deposit: ");
+            try {
+                finalDeposit = initialDeposit = (long) (sc.nextInt() * 100.0);
+            } catch (InputMismatchException e) {
+                System.out.println("\033[1;30m" + "\033[46m" + " Error, input data is not valid!" + "\u001B[0m");
+                sc.next();
+            }
+        } while (finalDeposit == 0);
 
-        long initialDeposit;
-        long finalDeposit = initialDeposit = (long)(sc.nextInt() * 100.0);
+        do {
+            System.out.println("Enter, please, interest rate: ");
+            try {
+                interestRate = sc.nextDouble();
+            } catch (InputMismatchException e)  {
+                System.out.println("\033[1;30m" + "\033[46m" + " Error, input data is not valid!" + "\u001B[0m");
+                sc.next();
+            }
 
-        System.out.println("Enter, please, interest rate: ");
-        double interestRate = sc.nextDouble();
+        } while (interestRate == 0);
 
-        System.out.println("Enter, please, your time term for a deposit, in years: ");
-        int termInYears = sc.nextInt();
+        do {
+            try {
+                System.out.println("Enter, please, your time term for a deposit, in years: ");
+                termInYears = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\033[1;30m" + "\033[46m" + " Error, input data is not valid!" + "\u001B[0m");
+                sc.next();
+            }
+        } while (termInYears == 0);
         long income;
 
         double coff = (1 + interestRate);
 
+        System.out.println("====================================");
         for (int i = 1; i <= termInYears; i++) {
             finalDeposit *= coff;
-            System.out.println(finalDeposit + " UAH to be settled on your account after year " + i);
+            System.out.println(finalDeposit / 100.0 + " UAH to be settled on your account after year " + i);
         }
 
         income = finalDeposit - initialDeposit;
 
+        System.out.println("====================================");
         System.out.println(income / 100.0 + " UAH is your total income");
 
 
