ead2e0d0b0a4d3b12fb44ebe08eb1fab9ca51be5
Atlantiks <atlantiks@bigmir.net>
2021-08-18 21:24:49 +0300

    Task 3 alpha

--- src/homework2/BankPercentage.java
+++ src/homework2/BankPercentage.java
@@ -10,24 +10,25 @@ public class BankPercentage {
         System.out.println("Enter, please, amount of your money for initial deposit: ");
 
         long initialDeposit;
-        long finalDeposit = initialDeposit = sc.nextInt();
+        long finalDeposit = initialDeposit = (long)(sc.nextInt() * 100.0);
 
+        System.out.println("Enter, please, interest rate: ");
+        double interestRate = sc.nextDouble();
 
-
-        double interestRate = 0.0575;
-
-        int termInYears = 2;
+        System.out.println("Enter, please, your time term for a deposit, in years: ");
+        int termInYears = sc.nextInt();
         long income;
 
         double coff = (1 + interestRate);
 
         for (int i = 1; i <= termInYears; i++) {
             finalDeposit *= coff;
+            System.out.println(finalDeposit + " UAH to be settled on your account after year " + i);
         }
 
         income = finalDeposit - initialDeposit;
 
-        System.out.println(income / 100.0);
+        System.out.println(income / 100.0 + " UAH is your total income");
 
 
     }
