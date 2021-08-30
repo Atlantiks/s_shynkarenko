52694fb14fd618db02ff16be593cb28478237c3e
Atlantiks <atlantiks@bigmir.net>
2021-08-30 19:17:28 +0300

    final draft locl

--- src/homework3/ChessBoard.java
+++ src/homework3/ChessBoard.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class ChessBoard {
     public static void main(String[] args) {
         String[][] board = new String[8][8];
+        System.currentTimeMillis();
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
