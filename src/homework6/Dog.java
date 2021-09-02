e636dcca335725d4d2181f9cf620cbf0947a889d
Atlantiks <atlantiks@bigmir.net>
2021-09-02 12:32:56 +0300

    Add voice() methods across al classes.

--- src/homework6/Dog.java
+++ src/homework6/Dog.java
@@ -1,4 +1,12 @@
 package homework6;
 
-public class Dog {
+public class Dog extends Domestic {
+
+    public Dog(int age, int weight, String color, String name, boolean isVaccinated) {
+        super(age, weight, color, name, isVaccinated);
+    }
+
+    public String getVoice() {
+        return super.getVoice() + "\nWoof\n";
+    }
 }
--- src/homework6/GuideDog.java
+++ src/homework6/GuideDog.java
@@ -1,6 +1,14 @@
 package homework6;
 
-public class GuideDog {
+public class GuideDog extends Dog {
     private boolean isTrained;
 
+    public GuideDog(int age, int weight, String color, String name, boolean isVaccinated, boolean isTrained) {
+        super(age, weight, color, name, isVaccinated);
+        this.isTrained = isTrained;
+    }
+
+    public String getVoice() {
+        return isTrained ? super.getVoice() + "I can take you home" : super.getVoice();
+    }
 }
