4943f0c9b2ee341d6bc097f410c190745422efdf
Atlantiks <atlantiks@bigmir.net>
2021-09-27 18:04:59 +0300

    Main class added.

--- src/homework9/TestMyCollection.java
+++ src/homework9/TestMyCollection.java
@@ -2,6 +2,8 @@ package homework9;
 
 public class TestMyCollection {
     public static void main(String[] args) {
+        MyLinkedList mylist = new MyLinkedList();
 
+        mylist.addAll(new String[]{"1","2","3","4","5"});
     }
 }
