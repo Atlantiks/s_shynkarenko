e08d24484785fff726d75d945416b970840c951a
Atlantiks <atlantiks@bigmir.net>
2021-10-04 10:05:02 +0300

    minor tests added.

--- src/homework9/TestMyCollection.java
+++ src/homework9/TestMyCollection.java
@@ -11,11 +11,13 @@ public class TestMyCollection {
 
         System.out.println(mylist.delete("2"));
         System.out.println("Size is: " + mylist.size());
-        System.out.println(mylist.delete(0));
+        //System.out.println(mylist.delete(0));
         System.out.println(mylist.get(0));
 
-
-
+        anotherMylist.addAll(mylist);
+        for (int i = 0; i < anotherMylist.size(); i++) {
+            System.out.println(anotherMylist.get(i));
+        }
 
     }
 }
