beb78d2fafb3a610cbf43baf7e66159e11685441
Atlantiks <atlantiks@bigmir.net>
2021-10-04 18:12:33 +0300

    Class "Box" added

---
+++ src/homework10/Box.java
@@ -0,0 +1,15 @@
+package homework10;
+
+public class Box {
+    int length;
+    int depth;
+    int height;
+
+    Box(int l, int d, int h) {
+        length = l;
+        depth = d;
+        height = h;
+    }
+
+
+}
