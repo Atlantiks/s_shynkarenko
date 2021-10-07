5303099bb79f1c0fa053055c9479872ca32121bb
Atlantiks <atlantiks@bigmir.net>
2021-10-07 03:23:07 +0300

    Task 3: updated console outputs.

--- src/homework10/WordRepeatsCounterInText.java
+++ src/homework10/WordRepeatsCounterInText.java
@@ -1,14 +1,13 @@
 package homework10;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordRepeatsCounterInText {
     public static void main(String[] args) {
         Map<String,Integer> allWordsCount = countRepeats(
-                "This immediately reveals that map.forEach() is also using Map.Entry internally. So I would not expect any performance benefit in using map.forEach() over the map.entrySet().forEach(). So in your case the answer really depends on your personal taste :)");
-        allWordsCount.forEach((key, value) -> System.out.println("Слово : " + key + " встречается " + value + " раз"));
+                "При сдаче после 11 октября от максимального балла будет отниматься штраф за просрочку.");
+        allWordsCount.forEach((key, value) -> System.out.println("Слово : " + key + "  встречается " + value + " раз"));
     }
 
     public static  Map<String,Integer> countRepeats(String text) {
