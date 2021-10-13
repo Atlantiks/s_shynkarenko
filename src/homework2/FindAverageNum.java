2b28b161229d1f6fa06b389e01998bc3fc2a25a7
Atlantiks <atlantiks@bigmir.net>
2021-10-13 19:06:36 +0300

    Homework 2: Task 2 re-done, exceptions handling included.

--- src/homework2/FindAverageNum.java
+++ src/homework2/FindAverageNum.java
@@ -1,22 +1,27 @@
 package homework2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FindAverageNum {
     public static void main(String[] args) {
-        int primaryNum, secondaryNum;
+        int primaryNum = 0;
+        int secondaryNum = 0;
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Please, enter two integer values: ");
-
-        primaryNum = sc.nextInt();
-        secondaryNum = sc.nextInt();
+        do {
+            System.out.println("Please, enter two integer values and press \"ENTER\": ");
+            try {
+                primaryNum = sc.nextInt();
+                secondaryNum = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Unsupported value entered. Please re-type.");
+            }
+        } while (primaryNum == 0 && secondaryNum == 0);
 
         sc.close();
-
         double averageNum = (primaryNum + secondaryNum) / 2.0;
-
         System.out.println(averageNum);
     }
 }
